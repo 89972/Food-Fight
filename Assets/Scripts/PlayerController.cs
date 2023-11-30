21e91d8b3d9fc3e7c3f06b6fc5668f21e67d55da
89972 <121228696+89972@users.noreply.github.com>
2023-11-30 11:42:13 +0100

    losing my will to live

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,18 @@
+using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.ObjectModel;
+using UnityEngine.Events;
+using System.Threading;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
 
 public class PlayerController : Entity
 {
+    public Hand hand;
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabGripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
     private Animator animator;
     private Camera mainCamera;
 
@@ -30,6 +39,17 @@ public class PlayerController : Entity
 
     void Update()
     {
+     if(grabGripAction.GetStateDown(handType))
+        {
+            SpawnObjectInHand();
+        }
+    }
+
+    private void SpawnObjectInHand()
+    {
+        //take a random fruit from a foodstand
+        GameObject fruit = Instantiate(GameManager.Instance.fruits[(int)currentFruit].prefab, transform.position, Quaternion.identity);
+        hand.AttachObject(fruit, GrabTypes.Grip);
         
     }
 }
